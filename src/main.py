864d402d8623e1b1218e5ecaf8cb49b567ee4f99
flysand7 <yyakut.ac@gmail.com>
2023-12-13 09:41:49 +1100

    add .gitignore

--- src/main.py
+++ src/main.py
@@ -4,15 +4,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 def root():
     return ""
 
 @app.get("/projects")
 def read_projects(project_id: int, q: Union[str, None] = None):
-    return []
+    _ = project_id
+    _ = q
+    return ["a"]
 
 @app.get("/projects/{project_id}")
 def read_project(project_id: int, q: Union[str, None] = None):
-    return [{}]
+    _ = project_id
+    _ = q
+    return [{"a": 2}]
